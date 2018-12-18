1729db0fdc1a06b2057b61e6a798831e6684e794
Chu Ningfan <chuqingning@163.com>
2018-12-18 20:37:38 +0800

    12

--- sso-service/src/main/java/sso/service/controller/AuthController.java
+++ sso-service/src/main/java/sso/service/controller/AuthController.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
